3cd798bc0a175ccebf7cb109869f1225d385dc34
DarioAnyelo <96701376+DarioAnyelo@users.noreply.github.com>
2026-06-19 05:20:42 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("saludar").addEventListener("click", function() {
+    document.getElementById("mensaje").innerHTML =
+        "¡Hola! Soy Anyelo Dario subi esta pequeña web.";
+});
